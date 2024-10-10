49594533e3c6d1263c6558d1f2a94a9c0436fd96
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2024-10-10 22:22:44 +0600

    updated coed

--- list.h
+++ list.h
@@ -22,7 +22,9 @@ class List {
     Node *head;
     Node *tail;
 
-
+    /*
+     * using searchHelper() private method to help searching an item in the linked list
+     */
     int searchHelper(Node * start, int key) {
         if (start == NULL) {
             return -1;
@@ -37,10 +39,22 @@ class List {
         return subIndex + 1;
     }
     public:
+
+    /*
+     * initializing a list in time of creation
+     */
     List():head(NULL),tail(NULL){}
+
+    /*
+     * creating a getter for getting head
+     */
     Node * getHead() {
         return head;
     }
+
+    /*
+     * implepenting push_front() method for unsorted linked list
+     */
     void push_front(int data) {
         if(head == NULL) {
             Node *n = new Node(data);
@@ -53,40 +67,120 @@ class List {
 
         }
     }
-    void push_back(int data) {
-        if(head == NULL) {
-            Node *n = new Node(data);
-            head = tail = n;
-        }
-        else {
-            Node *n = new Node(data);
-            tail->next = n;
-            tail = n;
-        }
-    }
-    void insert(int data, int pos) {
-        if(pos == 0) {
-            push_front(data);
-            return;
-        }
-        Node *temp = head;
-        for(int i = 1; i <= pos-1; i++) {
-            temp = temp->next;
-        }
-        Node *n = new Node(data);
-        n->next = temp->next;
-        temp->next = n;
-    }
+
+    /*
+     * implepenting push_back() method for unsorted linked list
+     */
+    // void push_back(int data) {
+    //     if(head == NULL) {
+    //         Node *n = new Node(data);
+    //         head = tail = n;
+    //     }
+    //     else {
+    //         Node *n = new Node(data);
+    //         tail->next = n;
+    //         tail = n;
+    //     }
+    // }
+
+
+    /*
+     * creating insert() method for unsorted linked list
+     */
+    // void insert(int data, int pos) {
+    //     if(pos == 0) {
+    //         push_front(data);
+    //         return;
+    //     }
+    //     Node *temp = head;
+    //     for(int i = 1; i <= pos-1; i++) {
+    //         temp = temp->next;
+    //     }
+    //     Node *n = new Node(data);
+    //     n->next = temp->next;
+    //     temp->next = n;
+    // }
 
 
     int recursiveSearch(int key) {
         int index = searchHelper(head, key);
         return index;
     }
+    /*
+     * creating destructor
+     */
     ~List() {
         if (head != NULL) {
             delete head;
             head = NULL;
         }
     }
+
+    /*
+     * inser
+     */
+    void insert(int data)
+    {
+        Node* current;
+        /* Special case for the head end */
+        if(head == NULL) {
+            Node *n = new Node(data);
+            head = tail = n;
+        }
+        else if (data < head->data) {
+            push_front(data);
+        }
+        else {
+            Node* n = new Node(data);
+            /* Locate the node before the
+            point of insertion */
+            current = head;
+            while (current->next != NULL  && current->next->data   < data) {
+                current = current->next;
+            }
+            n->next = current->next;
+            current->next = n;
+        }
+    }
+
+    void deleteHead() {
+        if (head != NULL) {
+            head = head->next;
+        }
+    }
+
+    void deleteItem(int item) {
+        if(item == head->data) {
+            deleteHead();
+        }
+        /*
+        *Detecting if the item is equals to tail. If so then just call the method deleteTail()
+        */
+        else if(item == tail->data) {
+            deleteTail();
+        }
+        else {
+            Node *current = head;
+            Node *prev = NULL;
+            Node *next = NULL;
+            while (current->next != NULL && current->next->data != item) {
+                current = current->next;
+                prev = current;
+                next = current->next->next;
+            }
+            prev->next = next;
+            current = nullptr;
+
+        }
+    }
+    void deleteTail() {
+        Node* second_last = head;
+        while (second_last->next->next != nullptr) {
+            second_last = second_last->next;
+        }
+        delete second_last->next;
+        second_last->next = nullptr;
+    }
+
+
 };
--- main.cpp
+++ main.cpp
@@ -4,19 +4,72 @@ using namespace std;
 
 int main() {
     List L;
-    L.push_front(1);
-    L.push_front(0);
-    L.push_back(3);
-    L.push_back(4);
-    L.insert(2, 2);
+    L.insert(10);
+    L.insert(5);
+    L.insert(9);
+    L.insert(7);
+    L.insert(6);
+    L.insert(30);
+    L.insert(33);
+    L.insert(25);
+
+    cout<<"printing list before any operation"<<endl;
 
     Node *h = L.getHead();
-    while (h != NULL) {
+    while (h != nullptr) {
         cout<<h->getData()<<" ";
         h = h->next;
     }
     cout<<endl;
 
-    cout<<L.recursiveSearch(3);
+    L.deleteItem(7);
+    L.deleteItem(5);
+
+    cout<<"printing list after deleting 5 and 7 from the list"<<endl;
+
+    h = L.getHead();
+    while (h != nullptr) {
+        cout<<h->getData()<<" ";
+        h = h->next;
+    }
+    cout<<endl;
+
+    L.deleteTail();
+    cout<<"printing list after deleting tail from the list"<<endl;
+
+    h = L.getHead();
+    while (h != nullptr) {
+        cout<<h->getData()<<" ";
+        h = h->next;
+    }
+    cout<<endl;
+
+    L.deleteHead();
+    cout<<"printing list after deleting head from the list"<<endl;
+
+    h = L.getHead();
+    while (h != nullptr) {
+        cout<<h->getData()<<" ";
+        h = h->next;
+    }
+    cout<<endl;
+
+    cout<<endl;
+
+    cout<<"printing result for searching 3 in the list"<<endl;
+    if (L.recursiveSearch(3) == 1) {
+        cout<<"found"<<endl;
+    }
+    else {
+        cout<<"not found"<<endl;
+    }
+
+    cout<<"printing result for searching 10 in the list"<<endl;
+    if (L.recursiveSearch(10) == 1) {
+        cout<<"found"<<endl;
+    }
+    else {
+        cout<<"not found"<<endl;
+    }
     return 0;
 }
